8d6164ebddf5096a12a5877e7f62409c9b309b42
mildock <youngwook@outlook.com>
2017-08-24 01:14:27 +0900

    Add chapter 4 Sample

---
+++ Book/Samples/Chapter 4/GreatWall4-2/GreatWall/Dialogs/RootDialog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace GreatWall.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        private string WelcomeMessage = "신속배달 만리장성 봇입니다. 1.주문 2.FAQ";
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync(WelcomeMessage);
+
+            context.Wait(SendWelcomeMessageAsync);
+        }
+
+        private async Task SendWelcomeMessageAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            string selected = activity.Text.Trim();
+
+            if (selected == "1")
+            {
+                await context.PostAsync("음식 주문 메뉴 입니다. 원하시는 음식을 입력해 주십시오.");
+                context.Call(new OrderDialog(), DialogResumeAfter);
+            }
+            else if (selected == "2")
+            {
+                await context.PostAsync("FAQ 서비스 입니다. 질문을 입력해 주십시오.");
+                
+            }
+            else
+            {
+                await context.PostAsync("잘못 선택하셨습니다. 다시 선택해 주십시오");
+
+            }
+            
+        }
+
+        private async Task DialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                string message = await result;
+
+                await context.PostAsync(WelcomeMessage); ;
+            }
+            catch (TooManyAttemptsException)
+            {
+                await context.PostAsync("오류가 생겼습니다. 죄송합니다.");
+            }
+        }
+    }
+}
