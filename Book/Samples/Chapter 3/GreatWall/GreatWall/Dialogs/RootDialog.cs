7589af572e939682b30d9a9f9db8331463b112d2
Young Wook Kim <yowkim@microsoft.com>
2017-08-23 23:34:36 +0900

    Add chapter 3 sample

---
+++ Book/Samples/Chapter 3/GreatWall/GreatWall/Dialogs/RootDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace GreatWall.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            string message = string.Format("{0}을 주문하셨습니다. 감사합니다.", activity.Text);
+
+            // return our reply to the user
+            await context.PostAsync(message);
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
