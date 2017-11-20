535c6e45100256ce1a10d34b3cd5819ee9069880
YoungWook Kim <youngwook@outlook.com>
2017-11-20 09:28:19 +0900

    7장 샘플 추가

---
+++ Book/Samples/Chapter 7/GreatWall7/GreatWall/Model/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreatWall.Model
+{
+    [Serializable]
+    public class OrderItem
+    {
+        public int ItemID { get; set; }
+        public string Title { get; set; }
+        public int Quantity { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
