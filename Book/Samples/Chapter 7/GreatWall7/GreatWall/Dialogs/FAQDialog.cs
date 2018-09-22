24c2dad9aa6e666065d56408c28c0c04bbf7be1e
KoreaEva <youngwook@outlook.com>
2018-09-22 11:11:16 +0900

    Update chapter 8

--- Book/Samples/Chapter 7/GreatWall7/GreatWall/Dialogs/FAQDialog.cs
+++ Book/Samples/Chapter 7/GreatWall7/GreatWall/Dialogs/FAQDialog.cs
@@ -14,13 +14,9 @@ using QnAMakerDialog;
 namespace GreatWall.Dialogs
 {
     [Serializable]
-    //[QnAMakerService("https://westus.api.cognitive.microsoft.com/qnamaker/v4.0","7a8abeed-12d4-4513-90f8-48cab30a42ee","592fd964-0e7b-4d9b-b343-6a15f27e429d",MaxAnswers = 5)]
-    [QnAMakerService("https://greatwallqna.azurewebsites.net/qnamaker","7a8abeed-12d4-4513-90f8-48cab30a42ee", "592fd964-0e7b-4d9b-b343-6a15f27e429d", MaxAnswers = 5)]
+    [QnAMakerService("https://greatwallqna.azurewebsites.net/qnamaker", "be9a25bc-715b-4ec7-a50f-4be894748f90", "9936be9a-840a-470c-a182-7735c78cbf27", MaxAnswers = 5)]
     public class FAQDialog : QnAMakerDialog<string>
     {
-        /// <summary>
-        /// Handler used when the QnAMaker finds no appropriate answer
-        /// </summary>
         public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
         {
             await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
@@ -35,20 +31,11 @@ namespace GreatWall.Dialogs
                 return;
             }
 
-            // ProcessResultAndCreateMessageActivity will remove any attachment markup from the results answer
-            // and add any attachments to a new message activity with the message activity text set by default
-            // to the answer property from the result
-            //var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
-            //messageActivity.Text = $"I found {result.Answers.Length} answer(s) that might help...here is the first, which returned a score of {result.Answers.First().Score}...{result.Answers.First().Answer}";
-
             await context.PostAsync(result.Answers.First().Answer);
 
             context.Wait(MessageReceived);
         }
 
-        /// <summary>
-        /// Handler to respond when QnAMakerResult score is a maximum of 0.5
-        /// </summary>
         [QnAMakerResponseHandler(0.5)]
         public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
         {
